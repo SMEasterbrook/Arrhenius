963ca1169d13dfdf90184701856cf43051860167
ahurka <hurka.alex@gmail.com>
2018-05-21 22:12:41 -0400

    Create file for data access functions

---
+++ data_provider.py
@@ -0,0 +1,92 @@
+import custom_data_readers
+from resources import DATASET_PATH, DATASETS
+
+import numpy as np
+
+"""
+This module contains prebuilt and custom data providers, functions
+that collect and format data from one of more datasets to construct a
+meaningful array of data.
+
+For instance, a provider function may access datasets on both land and
+ocean surface temperatures to produce a global surface temperature
+dataset, and do operations such as rearranging dimensions to make
+the datasets consistent.
+
+For every different data type needed, a new provider function should
+be made that is specialized to that data type and its origin.
+Additionally, stub or mock providers can be made for testing purposes.
+
+Data providers do not need to be associated with databases or
+datasets. Some may contain purely static values. These can be left
+as publicly exposed constants unless encapsulation reasons dictate
+otherwise.
+"""
+
+
+STATIC_ATM_PATHLENGTH = 1.61
+STATIC_ATM_CO2 = 408.39 / 1000000
+STATIC_ATM_H2O = None
+
+STATIC_ATM_ABSORBANCE = 0.70
+
+
+def berkeley_temperature_data() -> np.array:
+    """
+    A data provider returning temperature data from the Berkeley Earth
+    temperature dataset. Includes 100% surface and ocean coverage in
+    1-degree gridded format. Data is reported for the last full year,
+    in monthly sections.
+
+    Returned in a numpy array. First index corresponds to month, with
+    0 being January and 11 being December; the second index is latitude,
+    with 0 being 90 and 179 being 90; the third index is longitude,
+    specifications unknown.
+
+    :return: Berkeley Earth surface temperature data
+    """
+    temp_data_file = DATASET_PATH + DATASETS['temperature']
+    dataset = custom_data_readers.BerkeleyEarthTemperatureReader(
+        temp_data_file)
+
+    data = dataset.read_newest('temperature')
+    return data
+
+
+def static_albedo_data():
+    """
+    A data provider returning 1-degree gridded surface albedo data
+    for land and ocean. Uses Arrhenius' albedo scheme, in which all
+    land has a constant albedo, and all water likewise has a constant
+    albedo. In this case snow and clouds are ignored, although they
+    would contribute to lower global average albedo.
+
+    Data is returned in a numpy array. The first index represents
+    latitude, with 0 being 90 and 179 being 90; the seconds index is
+    longitude, specifications unknown.
+
+    :return: Static surface albedo data by Arrhenius' scheme
+    """
+    temp_data_file = DATASET_PATH + DATASETS['temperature']
+    dataset = custom_data_readers.BerkeleyEarthTemperatureReader(
+        temp_data_file)
+
+    # Berkeley Earth dataset includes variables indicating which 1-degree
+    # latitude-longitude cells are primarily land.
+    land_coords = dataset.read_newest('land_mask')
+    # Land values have an albedo of 1. Fill in all for now.
+    land_mask = np.ones((180, 360), dtype=float)
+
+    # Correct array with the proper albedo in ocean cells.
+    ocean_albedo = (1 - 0.075)
+    for i in range(180):
+        # Cache intermediary arrays to prevent excess indexing operations.
+        coord_row = land_coords[i]
+        mask_row = land_mask[i]
+
+        for j in range(360):
+            if coord_row[j] == 0:
+                # Represents a primarily ocean-covered grid cell.
+                mask_row[j] = ocean_albedo
+
+    return land_mask
