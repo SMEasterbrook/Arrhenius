f8a1f77783dd6bd2ff94c4f098aef7b3e51ffea7
ahurka <hurka.alex@gmail.com>
2018-05-21 22:24:06 -0400

    Reconfigure resource dataset levels

--- resources.py
+++ resources.py
@@ -1,8 +1,10 @@
 DATASET_PATH = 'models/'
 
 DATASETS = {
-    'temperature': 'Land_And_Ocean_LatLong1.nc',
+    'temperature': {
+        'berkeley': 'Land_And_Ocean_LatLong1.nc'
+    },
     'albedo': None,
-    'carbon': 'CT2016.molefrac_nam1x1_2016-01-01',
+    'carbon': None,
     'water': None
 }
