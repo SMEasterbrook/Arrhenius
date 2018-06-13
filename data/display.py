abcc7a5dc7c14799e20adf02c8be46ed44b28839
ahurka <hurka.alex@gmail.com>
2018-06-12 23:04:38 -0400

    Add output center and organizer
    The ModelOutput class is responsible for producing and organizing all
    program output, including image files and NetCDF data files. Currently
    only documentation is present, without any functionality.

--- data/display.py
+++ data/display.py
@@ -1,7 +1,14 @@
+import os
 from typing import List, Tuple, Union
 
+from resources import OUTPUT_REL_PATH
+
+from pathlib import Path
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
+
+
+OUTPUT_FULL_PATH = os.path.join(Path('.').absolute(), OUTPUT_REL_PATH)
 
 
 class ModelImageRenderer:
@@ -71,4 +78,61 @@ class ModelImageRenderer:
         # Save the image and clear added components from memory
         plt.savefig(out_path)
         plt.close()
+
+
+class ModelOutput:
+    """
+    A general-purpose center for all forms of output. Responsible for
+    organization of program output into folders.
+
+    The output of a program is defined as the temperature data produced by
+    a run of the model. This data may be saved in the form of a data file
+    (such as a NetCDF file) and/or as image representations, and/or in other
+    data formats.
+
+    All of these output types are produced side-by-side, and stored in their
+    own directory to keep data separate from different model runs.
+    """
+
+    def __init__(self: 'ModelOutput',
+                 title: str,
+                 data: List[Union[List, float]],
+                 data_grid: Tuple[int]) -> None:
+        """
+        Instantiate a new ModelOutput object.
+
+        Model data is provided through the data parameter, which must be an
+        array or array-like structure (such as a nested list) of the right
+        dimensions. The first dimension is time, and can contain any number
+        of elements; the second and third dimensions are latitude and
+        longitude, respectively. The number of elements in these dimensions
+        must be able to be multiplied by the corresponding grid cell width
+        in the grid to gve 180 degrees latitude and 360 degrees longitude.
+
+        The grid must be a tuple of two numbers which represent the width of
+        each grid cell in degrees latitude and longitude, respectively.
+
+        Finally, the title parameter gives the name of the directory that
+        will store output files. This directory will be located within a
+        directory called 'output' within the program's running directory.
+        Output files will also be named after this title, with possible
+        deviations, for example to denote between different seasons amongst
+        a set of output files that each represents a season.
+
+        :param title:
+            The base name of the output files, and of the directory that will
+            store them
+        :param data:
+            An array-like structure of numeric values, representing temperature
+            over a globe
+        :param data_grid:
+            The latitude and longitude widths of a cell in the data's grid
+        """
+        # Create output directory if it does not already exist.
+        parent_out_dir = Path(OUTPUT_FULL_PATH)
+        parent_out_dir.mkdir(exist_ok=True)
+
+        self._title = title
+        self._data = data
+        self._grid = data_grid
 
