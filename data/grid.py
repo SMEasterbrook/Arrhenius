d2492feb68a6e94c8005423eb3bba69fec932875
ahurka <hurka.alex@gmail.com>
2018-06-12 23:39:11 -0400

    Added grid cell data class

--- data/grid.py
+++ data/grid.py
@@ -30,3 +30,85 @@ def convert_grid_format(grid):
         new_lon = int(360 / grid[1])
 
         return new_lat, new_lon
+
+
+class GridCell:
+    """
+    A single cell within a latitude-longitude grid overlaid over Planet Earth.
+
+    Each cell has a temperature, a relative humidity value, and an albedo.
+    Where applicable, a grid cell represents only a single atmospheric layer,
+    so that multiple grid cells may occupy the same latitude and longitude
+    if they have different latitudes.
+    """
+    def __init__(self: 'GridCell',
+                 temp: float,
+                 r_hum: float,
+                 albedo: float) -> None:
+        """
+        Instantiate a new GridCell object with values for all the variables
+        within the grid cell. Location (latitude/longitude ranges) and size
+        are not tracked inside the object, and must be maintained externally
+        if they are required.
+
+        :param temp:
+            Average temperature, in degrees Celsius, within the grid cell
+        :param r_hum:
+            Average relative humidity within the grid cell
+        :param albedo:
+            Average surface albedo within the grid cell
+        """
+        self._temperature = temp
+        self._rel_humidity = r_hum
+        self._albedo = albedo
+
+    def get_temperature(self: 'GridCell') -> float:
+        """
+        Returns the average temperature of this grid cell.
+
+        :return:
+            The average temperature within the cell, in degrees Celsius
+        """
+        return self._temperature
+
+    def set_temperature(self: 'GridCell',
+                        new_temp: float) -> None:
+        """
+        Set a new value for the temperature within this grid cell.
+
+        :param new_temp:
+            The new value, in degrees Celsius, for temperature in this cell
+        """
+        self._temperature = new_temp
+
+    def get_relative_humidity(self: 'GridCell') -> float:
+        """
+        Returns the average relative humidity of this grid cell.
+
+        :return:
+            The average relative humidity of this grid cell
+        """
+        return self._rel_humidity
+
+    def set_relative_humidity(self: 'GridCell',
+                              new_r_hum: float) -> None:
+        """
+        Set a new value for the relative humidity within this grid cell.
+
+        :param new_r_hum:
+            The new value for relative humidity in this cell
+        """
+        self._rel_humidity = new_r_hum
+
+    def get_albedo(self: 'GridCell') -> float:
+        """
+        Returns the average surface albedo of this grid cell. Zero denotes
+        that the cell reflects all light, and 1 that it absorbs all light.
+
+        Non-surface grid cells (those at a non-zero altitude) have 0 albedo,
+        since cloud albedo is ignored.
+
+        :return:
+            The average surface albedo within the cell
+        """
+        return self._albedo
