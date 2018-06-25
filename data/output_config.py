ff6be518602b9cacf44ca40d2cc3bf9c576c75ab
ahurka <hurka.alex@gmail.com>
2018-06-24 23:23:16 -0400

    Add collection output handling

--- data/output_config.py
+++ data/output_config.py
@@ -1,11 +1,12 @@
 from enum import Enum, auto
-from typing import Callable
+from typing import Tuple, Dict, Callable, Optional
 
 from data.configuration import Config
 
 
-OUTPUT_TYPE_HANDLER = Callable[[str, object], None]
-COLLECTION_HANDLER = Callable[[str, Config, object]]
+# Type aliases
+OutputTypeHandler = Callable[[str, object], None]
+CollectionHandler = Callable[[str, Config, object]]
 
 
 class OutputConfig(Enum):
@@ -80,6 +81,11 @@ class Debug(OutputConfig):
     PRINT_NOTICES = auto()
 
 
+# Keys in collection dictionaries
+COLLECTION_SUBTYPES = "Contents"
+COLLECTION_HANDLERS = "Handlers"
+
+
 class OutputController:
     """
     An control point for model output. Acts as a logging center, allowing
@@ -118,9 +124,25 @@ class OutputController:
         self._collections = {}
         self._output_types = {}
 
+    def _navigate_collection_path(self: 'OutputController',
+                                  collection_path: Tuple[str]) -> Dict:
+        """
+        Returns the collection structure specified by the single argument.
+
+        :param collection_path:
+            A path to the collection in question through its ancestors
+        :return:
+            The collection defined by the path
+        """
+        collection = self._collections
+        for collection_name in collection_path:
+            collection = collection[collection_name]
+
+        return collection
+
     def enable_output_type(self: 'OutputController',
                            output_type: 'OutputConfig',
-                           handler: OUTPUT_TYPE_HANDLER = print) -> None:
+                           handler: OutputTypeHandler = print) -> None:
         """
         Register an output type to allow its output to be processed.
 
@@ -140,7 +162,7 @@ class OutputController:
         """
         self._output_types[output_type] = handler
 
-    def record_output(self: 'OutputController',
+    def submit_output(self: 'OutputController',
                       output_type: 'OutputConfig',
                       data: object) -> None:
         """
@@ -160,3 +182,104 @@ class OutputController:
         if output_type in self._output_types:
             handler = self._output_types[output_type]
             handler(self._title, data)
+
+    def register_collection(self: 'OutputController',
+                            collection_name: str,
+                            supercollections: Tuple[str] = (),
+                            handler: Optional[CollectionHandler] = None) -> None:
+        """
+        Creates a new collection in the collection directory, without any
+        output types registered in it.
+
+        If the collection already exists, it is not overwritten, but its
+        handler function may be replaced if a new one is provided.
+
+        A collection can have contain three types of structures: it can hold
+        a set of output types that are considered active; it can hold a
+        handler function that is invoked on any data that is associated with
+        the collection; and it can hold further collections inside of it.
+
+        The optional supercollections parameter specifies the ancestor
+        collections under which the new one is to be created, in order of
+        appearance in the collections directory structure. If not provided,
+        the new collection will be created at the top level of the directory.
+
+        The optional handler parameter gives the option to specify a handler
+        function that is invoked when data is associated with the collection.
+        If not provided, the collection will not have a handler, which is
+        permissible as long as one of its ancestors has a handler.
+
+        :param collection_name:
+            The name of the new collection
+        :param supercollections:
+            A path to the parent of the new collection, through its ancestors
+        :param handler:
+            A function that will be called on any output to this collection
+        """
+        parent_collection = self._navigate_collection_path(supercollections)
+
+        # Add the new collection if not present, but do not overwrite if
+        # already present.
+        if collection_name in parent_collection:
+            collection = parent_collection[collection_name]
+        else:
+            collection = {}
+            parent_collection[collection_name] = collection
+
+        # Only add a handler entry into the dict if a handler is specified.
+        if handler is not None:
+            collection[COLLECTION_HANDLERS] = handler
+
+    def enable_collection_type(self: 'OutputController',
+                               collection_path: Tuple[str],
+                               output_type: 'OutputConfig') -> None:
+        """
+        Record that a given output type is enabled within a collection.
+
+        This output type will be allowed when data is received that is
+        associated with the collection. However, how the data is output
+        depends on the collection's handler function, and may not be
+        specified for the output type alone.
+
+        :param collection_path:
+            A path to the collection in question through its ancestors
+        :param output_type:
+            The type of output being registered
+        """
+        collection = self._navigate_collection_path(collection_path)
+
+        # Accumulate output types in a set. The set may not be present when
+        # the first output type is registered, and may need to be created.
+        if COLLECTION_SUBTYPES not in collection:
+            collection[COLLECTION_SUBTYPES] = {output_type}
+        else:
+            collection[COLLECTION_SUBTYPES].add(output_type)
+
+    def submit_collection_output(self: 'OutputController',
+                                 collection_path: Tuple[str],
+                                 data: object) -> None:
+        """
+        Submit data for output, associated with the collection described by
+        the collection path argument.
+
+        This operation is only valid if a handler function has been registered
+        with the collection in question. If so, this handler is invoked with
+        the model run's title as its first argument, a dict containing the
+        collection's substructure as its second argument, and the data as its
+        third argument.
+
+        The second argument, the collection structure, contains information
+        such as child collections, and a set of output types that are enabled
+        within the collection.
+
+        Precondition:
+            A handler has been registered with this collection.
+
+        :param collection_path:
+            A path to the collection in question through its ancestors
+        :param data:
+            Data to be output
+        """
+        collection = self._navigate_collection_path(collection_path)
+        handler = collection[COLLECTION_HANDLERS]
+        handler(self._title, collection, data)
