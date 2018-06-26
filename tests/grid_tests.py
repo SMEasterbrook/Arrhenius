30272383fbb0c378bcde5d6a48c8d7b5e1c2a631
ahurka <hurka.alex@gmail.com>
2018-06-25 20:24:39 -0400

    Add GridCell tests

--- tests/grid_tests.py
+++ tests/grid_tests.py
@@ -1,5 +1,5 @@
 import unittest
-from data.grid import GridDimensions
+from data.grid import GridDimensions, GridCell, LatLongGrid
 
 
 class GridDimensionsTest(unittest.TestCase):
@@ -231,3 +231,156 @@ class GridDimensionsTest(unittest.TestCase):
         """
         with self.assertRaises(ValueError):
             grid = GridDimensions((10, 0.74))
+
+
+class GridCellTest(unittest.TestCase):
+    """
+    A test class for GridCell. Ensures correct data tracking, including
+    changes in any variables that are tracked.
+    """
+
+    def test_valid_init(self):
+        """
+        Test error-free creation of a valid grid cell, and proper returns from
+        getter methods.
+        """
+        temp = 1
+        r_hum = 75
+        albedo = 1
+
+        cell = GridCell(temp, r_hum, albedo)
+
+        self.assertEqual(cell.get_temperature(), temp)
+        self.assertEqual(cell.get_relative_humidity(), r_hum)
+        self.assertEqual(cell.get_albedo(), albedo)
+        self.assertEqual(cell.get_temperature_change(), 0)
+
+    def test_temperature_change_tracks(self):
+        """
+        Test that changes to a grid cell's temperature are reflected properly
+        in both its temperature value and temperature change value.
+        """
+        temp = 298
+        r_hum = 37.25
+        albedo = 0.5
+
+        cell = GridCell(temp, r_hum, albedo)
+        self.assertEqual(cell.get_temperature_change(), 0)
+
+        cell.set_temperature(temp + 1)
+        self.assertEqual(cell.get_temperature(), temp + 1)
+        self.assertEqual(cell.get_temperature_change(), 1)
+
+        cell.set_temperature(temp - 1)
+        self.assertEqual(cell.get_temperature(), temp - 1)
+        self.assertEqual(cell.get_temperature_change(), -1)
+
+    def test_negative_temp_error(self):
+        """
+        Test that attempting to create a grid cell with negative temperature
+        results in an error.
+        """
+        with self.assertRaises(ValueError):
+            cell = GridCell(-50, 50, 0.1)
+
+        with self.assertRaises(ValueError):
+            cell = GridCell(-0.01, 25, 0.36)
+
+    def test_negative_humidity_error(self):
+        """
+        Test that attempting to create a grid cell with negative relative
+        humidity results in an error.
+        """
+        with self.assertRaises(ValueError):
+            cell = GridCell(12, -62, 0.67)
+
+        with self.assertRaises(ValueError):
+            cell = GridCell(162, -0.01, 0.925)
+
+    def test_out_of_bounds_humidity_error(self):
+        """
+        Test that attempting to create a grid cell with relative humidity
+        above 100% results in an error.
+        """
+        with self.assertRaises(ValueError):
+            cell = GridCell(26, 125, 0.22)
+
+        with self.assertRaises(ValueError):
+            cell = GridCell(83, 100.01, 0.4)
+
+    def test_negative_albedo_error(self):
+        """
+        Test that attempting to create a grid cell with negative albedo
+        results in an error.
+        """
+        with self.assertRaises(ValueError):
+            cell = GridCell(37, 25, -1)
+
+        with self.assertRaises(ValueError):
+            cell = GridCell(70.6, 1.9, -0.01)
+
+    def test_out_of_bounds_albedo_error(self):
+        """
+        Test that attempting to create a grid cell with albedo greater than 1
+        results in an error.
+        """
+        with self.assertRaises(ValueError):
+            cell = GridCell(87, 45, 2)
+
+        with self.assertRaises(ValueError):
+            cell = GridCell(51.6, 25.2, 1.01)
+
+    def test_low_border_values(self):
+        """
+        Test that a grid cell can be created without error using low border
+        values for all three variables, namely 0 for temperature, humidity,
+        and albedo.
+        """
+        cell = GridCell(0, 0, 0)
+
+        self.assertEqual(cell.get_temperature(), 0)
+        self.assertEqual(cell.get_relative_humidity(), 0)
+        self.assertEqual(cell.get_albedo(), 0)
+
+    def test_high_border_values(self):
+        """
+        Test that a grid cell can be created without error using high border
+        values for all three variables, using 100 for humidity and 1 for
+        albedo. Temperature has no high border.
+        """
+        cell = GridCell(987654321, 100, 1)
+
+        self.assertEqual(cell.get_temperature(), 987654321)
+        self.assertEqual(cell.get_relative_humidity(), 100)
+        self.assertEqual(cell.get_albedo(), 1)
+
+    def test_invalid_temperature_change(self):
+        """
+        Test that an error is raised when a grid cell's temperature is changed
+        to an invalid value after the cell is instantiated. Also ensures that
+        the grid cell's temperature is unchanged after the attempt.
+        """
+        cell = GridCell(273.15, 65, 0.6)
+        cell.set_temperature(274.15)
+
+        with self.assertRaises(ValueError):
+            cell.set_temperature(-1)
+
+        self.assertEqual(cell.get_temperature(), 274.15)
+        self.assertEqual(cell.get_temperature_change(), 1)
+
+    def test_invalid_humidity_change(self):
+        """
+        Test that an error is raised when a grid cell's relative humidity is
+        changed to an invalid value after the cell is instantiated. Also
+        ensures that the grid cell's humidity is unchanged after the attempt.
+        """
+        cell = GridCell(273.15, 65, 0.6)
+
+        with self.assertRaises(ValueError):
+            cell.set_relative_humidity(-0.01)
+
+        with self.assertRaises(ValueError):
+            cell.set_relative_humidity(100.1)
+
+        self.assertEqual(cell.get_relative_humidity(), 65)
