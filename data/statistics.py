0eb91ae9c9a491be4520bf15a069903dcbd04906
ahurka <hurka.alex@gmail.com>
2018-07-06 22:25:25 -0400

    Init statistics module

---
+++ data/statistics.py
@@ -0,0 +1,6 @@
+"""
+A module for data processing and internal data display functions.
+Provides functions that manipulate gridded data into other forms, display
+grids as tables, and extract interesting statistics from gridded data such
+as sample means.
+"""
