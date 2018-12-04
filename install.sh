50dbfd6ac671c3a71122812179463878ae86c5a5
Steve Easterbrook <sme@cs.toronto.edu>
2018-12-04 14:12:49 -0500

    modified install.sh to add calls to wget to download the third party data sets

--- install.sh
+++ install.sh
@@ -17,3 +17,8 @@ pip install -e .
 # Write environment variables that are used by the project.
 export PYTHONHASHSEED=0
 export ARRHENIUS_MAIN_PATH=`pwd`
+
+# Download data files from remote sources
+wget -nc -P data/models http://berkeleyearth.lbl.gov/auto/Global/Gridded/Land_and_Ocean_LatLong1.nc
+wget -nc -P data/models ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.derived/pressure/air.mon.mean.nc
+wget -nc -P data/models ftp://ftp.cdc.noaa.gov/Datasets/ncep.reanalysis.derived/pressure/rhum.mon.mean.nc
